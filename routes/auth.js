7eee545cb6454b60899828c119d79627a54b12a4
adeel-alethis <adeel.baig@alethea.ui>
2024-11-27 18:50:55 +0500

    invalid credention error

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,7 @@ router.post('/login', async (req, res) => {
         if (!user) return res.status(400).json({ message: 'User not found' });
 
         const isMatch = await user.matchPassword(password);
-        if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+        if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
 
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
